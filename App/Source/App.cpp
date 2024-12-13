abe914e22f8694fc174a157ff2f5cf0bde5fbaf2
Sayed Abdulmohsen Alhashemi <s.abdulalhash@gmail.com>
2024-12-13 17:38:35 +0300

    Project Restructure

---
+++ App/Source/App.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "Renderer/Renderer.h"
+#include "Renderer/API/Vulkan/Components/Window/WindowHandler.h"
+
+int main() {
+    try {
+        WindowHandler* window = new WindowHandler();
+        window->Create(600, 600);
+
+        Renderer* renderer = new Renderer(window);
+        renderer->CreateAPIContext();
+
+        window->SetApiContext(renderer->vulkanContext);
+
+        renderer->RenderLoop();
+    }
+    catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
