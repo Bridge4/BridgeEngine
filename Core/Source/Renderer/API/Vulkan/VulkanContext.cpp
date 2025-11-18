901e4c706aab91d02d95492993f16d1792f1d8de
Bridge <s.abdulalhash@gmail.com>
2025-11-19 00:02:47 +0100

    modified CreateDescriptorPool to work with meshes

--- Core/Source/Renderer/API/Vulkan/VulkanContext.cpp
+++ Core/Source/Renderer/API/Vulkan/VulkanContext.cpp
@@ -468,17 +468,18 @@ void VulkanContext::CreateDescriptorPool() {
         Some will let you get away with bad allocations, some won't. Be careful.
     */
 
+    uint32_t poolSize = MAX_FRAMES_IN_FLIGHT * m_vulkanInstanceManager->m_meshList.size();
     std::array<VkDescriptorPoolSize, 2> poolSizes{};
     poolSizes[0].type = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
-    poolSizes[0].descriptorCount = static_cast<uint32_t>(MAX_FRAMES_IN_FLIGHT);
+    poolSizes[0].descriptorCount = poolSize;
     poolSizes[1].type = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER;
-    poolSizes[1].descriptorCount = static_cast<uint32_t>(MAX_FRAMES_IN_FLIGHT);
+    poolSizes[1].descriptorCount = poolSize;
 
     VkDescriptorPoolCreateInfo poolInfo{};
     poolInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_POOL_CREATE_INFO;
     poolInfo.poolSizeCount = static_cast<uint32_t> (poolSizes.size());
     poolInfo.pPoolSizes = poolSizes.data();
-    poolInfo.maxSets = static_cast<uint32_t>(MAX_FRAMES_IN_FLIGHT);
+    poolInfo.maxSets = poolSize;
 
     if (vkCreateDescriptorPool(*m_vulkanInstanceManager->GetRefLogicalDevice(), &poolInfo, nullptr, &m_vulkanInstanceManager->m_descriptorPool) != VK_SUCCESS) {
         throw std::runtime_error("failed to create descriptor pool!");
