09edcb91dee2a4e43e838401e4a566fa051c04f2
Bridge <s.abdulalhash@gmail.com>
2025-11-18 13:07:36 +0100

    :q

--- Core/Source/Renderer/API/Vulkan/VulkanContext.h
+++ Core/Source/Renderer/API/Vulkan/VulkanContext.h
@@ -79,7 +79,7 @@ public:
     uint32_t WIDTH = 800;
     uint32_t HEIGHT = 600;
     std::string MODEL_PATH = "C:/Source/Engines/BridgeEngine/Models/VikingRoom/VikingRoom.obj";
-    std::string TEXTURE_PATH = "C:/Source/Engines/BridgeEngine/Models/VikingRoom/VikingRoom.obj";
+    std::string TEXTURE_PATH = "C:/Source/Engines/BridgeEngine/Models/VikingRoom/VikingRoom.png";
     const std::vector<const char*> validationLayers = { "VK_LAYER_KHRONOS_validation" };
     const std::vector<const char*> deviceExtensions = { VK_KHR_SWAPCHAIN_EXTENSION_NAME };
     VkInstance m_instance;
