dd3cd4c5bc0c104d354473ad9b97608c3b48ea70
Bridge <s.abdulalhash@gmail.com>
2025-11-28 00:49:39 +0100

    changed background color to black

--- Core/Source/Renderer/API/Vulkan/VulkanContext.cpp
+++ Core/Source/Renderer/API/Vulkan/VulkanContext.cpp
@@ -600,7 +600,7 @@ void VulkanContext::recordCommandBuffer(VkCommandBuffer commandBuffer, uint32_t 
 
     // VK_ATTACHMENT_LOAD_OP_3 clear values for color and depth stencil
     std::array<VkClearValue, 2> clearValues{};
-    clearValues[0].color = { {1.0f, 0.2331f, 0.1555f, 1.0f} };
+    clearValues[0].color = { {0.0f, 0.0f, 0.0f, 1.0f} };
     clearValues[1].depthStencil = { 1.0f, 0 };
     renderPassInfo.clearValueCount = static_cast<uint32_t>(clearValues.size());
     renderPassInfo.pClearValues = clearValues.data();
